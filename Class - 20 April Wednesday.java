fd69ef4525f8ae4c9ae33e78a26c137b854f30a2
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-04-30 15:13:09 +0530

    Create Class - 20 April Wednesday.java

---
+++ Class - 20 April Wednesday.java
@@ -0,0 +1,79 @@
+public class Main {
+    // public static void changeGear(){
+    //     System.out.println("Gear is automatically changed");
+    // }
+    
+    // public static datareturntype functionname
+    // Function Declaration or Function Definition (How)
+    
+    
+//     public static int add(int a, int b){
+//         int res = a + b;
+//         return res;
+//     }
+    
+      public static int fun(){
+          return 10;
+      }
+    
+    // public static void greetingTheWorld() {
+    //     System.out.println("Hello World");
+    // }
+    
+    public static void main(String[] args) {
+        // System.out.println(fun());
+        int res = fun();
+        
+        // int res = add(4, 6);
+        // System.out.println(res);
+        // getRandom();
+        // Scanner scn = new Scanner(System.in);
+        // int a = scn.nextInt();
+        // int b = scn.nextInt();
+        // add(a, b);
+        // add(1);
+        // add();
+        
+        // System.out.println("I am before the greeting world");
+        // greetingTheWorld();
+        // System.out.println("I am after the greeting world");
+        
+            // Scanner scn = new Scanner(System.in);
+            // int n = scn.nextInt();    
+            // isPrime(n); // Function calling
+//         // Concept: Methods or Functions in Java
+        // greetingTheWorld(); // Function Calling or Function Invokation
+        // Executing the Code
+        
+//         Scanner scn = new Scanner(System.in);
+// //         int a = scn.nextInt();
+// //         int b = scn.nextInt();
+// //         int c = scn.nextInt();
+// //         int d = scn.nextInt();
+// //         int e = scn.nextInt();
+        
+        
+// //         int res = add(a, b);
+// //         res = add(res, c);
+// //         res = add(res, d);
+// //         res = add(res, e);
+// //         System.out.println(res);
+        
+//         changeGear();
+//         changeGear();
+//         changeGear();
+        
+//         // Types of Functions:
+// 1. W/O Parameter, W/O Return Type
+//         // 2. With Parameter, W/O Return Type
+//         // (Passing Primitive Datatypes)
+            
+        
+//         // 3. W/O Parameter, With Return Type
+//         // 4. With Parameter, With Return Type
+//         // 
+        
+//         // Recursion/Function Call Stack Diagram
+//         // Swapping Game
+    }
+}
